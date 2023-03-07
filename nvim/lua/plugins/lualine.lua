713fcd82217afd8b49146aa12e2b862f6df1146e
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-03-07 10:57:53 +0100

    nvim mod fixes lualine theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 require('lualine').setup({
   options = {
-    theme = string.format('%s', color_theme),
+    theme = string.format('codedark', color_theme),
     icons_enabled = true,
     section_separators = '',
     component_separators = ''
