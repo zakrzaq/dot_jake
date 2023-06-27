1a0ac188d088fd3f37105626fad50952b8d80d8c
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-06-27 11:31:32 +0200

    setup including link scripts added fix codium auth script

---
+++ scripts/fix-codeium-nvim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nvim ~/.local/share/nvim/lazy/codeium.nvim/lua/codeium/views/auth-menu.lua
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf ~/dot_jake/.zshrc ~/
+ln -sf ~/dot_jake/git/.gitconfig ~/
+ln -sf ~/dot_jake/.tmux.conf ~/
+ln -sf ~/dot_jake/nvim/* ~/.config/nvim/
+ln -sf ~/dot_jake/scripts/* ~/.local/bin/
+
