ee3a9a883b75b083e7f1286a4630efc000be4676
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-08-01 10:01:46 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -sf ~/dot_jake/.zshrc ~/
-ln -sf ~/dot_jake/git/.gitconfig ~/
+ln -sf ~/dot_jake/.gitconfig ~/
 ln -sf ~/dot_jake/.tmux.conf ~/
 ln -sf ~/dot_jake/nvim/* ~/.config/nvim/
 ln -sf ~/dot_jake/scripts/* ~/.local/bin/
