5da9a80edce7979abcc415c1c878c7775962e3bf
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-08-08 06:32:44 +0200

    setup.sh incl kitty

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 ln -sf ~/dot_jake/.zshrc ~/
 ln -sf ~/dot_jake/.gitconfig ~/
 ln -sf ~/dot_jake/.tmux.conf ~/
+mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.bak
+ln -sf ~/dot_jake/kitty.conf ~/.config/kitty/
 ln -sf ~/dot_jake/nvim/* ~/.config/nvim/
 ln -sf ~/dot_jake/scripts/* ~/.local/bin/
 
